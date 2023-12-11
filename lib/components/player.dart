c8e70734c8bff76df4349c22aeb301257015d8bc
Lwin <lwindev97@gmail.com>
2023-12-11 12:27:30 +0630

    Collision Detection

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,12 +1,16 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 
 import '../helpers/direction.dart';
 
+import 'world_collidable.dart';
+
 // Extends SpriteAnimationComponent to set an active animation in your game.
 // By using the HasGameRef mixin,
 // the Player now has access to the core functionality of the Flame engine.
-class Player extends SpriteAnimationComponent with HasGameRef {
+class Player extends SpriteAnimationComponent
+    with HasGameRef, CollisionCallbacks {
   Direction direction = Direction.none;
   final double _playerSpeed = 300.0;
   final double _animationSpeed = 0.15;
@@ -16,16 +20,41 @@ class Player extends SpriteAnimationComponent with HasGameRef {
   late final SpriteAnimation _runRightAnimation;
   late final SpriteAnimation _standingAnimation;
 
+  // Variables to help track collisions
+  Direction _collisionDirection = Direction.none;
+  bool _hasCollided = false;
+
   Player()
       : super(
           size: Vector2.all(50.0), // The size of player is 50
-        );
+        ) {
+    add(RectangleHitbox());
+  }
+  // Like WorldCollision components, your player needs a HitBox to be able to
+  // register collisions.
 
   // "update" is a function unique to Flame components.
   // It will be called each time a frame must be rendered, and
   // Flame will ensure all your game components update at the same time.
   // The delta represents how much time has passed since the last update cycle
   // and can be used to move the player predictably.
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    if (other is WorldCollidable) {
+      if (!_hasCollided) {
+        _hasCollided = true;
+        _collisionDirection = direction;
+      }
+    }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    super.onCollisionEnd(other);
+    _hasCollided = false;
+  }
 
   @override
   void update(double delta) {
@@ -36,20 +65,28 @@ class Player extends SpriteAnimationComponent with HasGameRef {
   void movePlayer(double delta) {
     switch (direction) {
       case Direction.up:
-        animation = _runUpAnimation;
-        moveUp(delta);
+        if (canPlayerMoveUp()) {
+          animation = _runUpAnimation;
+          moveUp(delta);
+        }
         break;
       case Direction.down:
-        animation = _runDownAnimation;
-        moveDown(delta);
+        if (canPlayerMoveDown()) {
+          animation = _runDownAnimation;
+          moveDown(delta);
+        }
         break;
       case Direction.left:
-        animation = _runLeftAnimation;
-        moveLeft(delta);
+        if (canPlayerMoveLeft()) {
+          animation = _runLeftAnimation;
+          moveLeft(delta);
+        }
         break;
       case Direction.right:
-        animation = _runRightAnimation;
-        moveRight(delta);
+        if (canPlayerMoveRight()) {
+          animation = _runRightAnimation;
+          moveRight(delta);
+        }
         break;
       case Direction.none:
         animation = _standingAnimation;
@@ -98,5 +135,36 @@ class Player extends SpriteAnimationComponent with HasGameRef {
 
   void moveRight(double delta) {
     position.add(Vector2(delta * _playerSpeed, 0));
+  }
+
+  // These methods will check whether the player can move in a given direction
+  // by querying the collision variables you created.
+
+  bool canPlayerMoveUp() {
+    if (_hasCollided && _collisionDirection == Direction.up) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveDown() {
+    if (_hasCollided && _collisionDirection == Direction.down) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveLeft() {
+    if (_hasCollided && _collisionDirection == Direction.left) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveRight() {
+    if (_hasCollided && _collisionDirection == Direction.right) {
+      return false;
+    }
+    return true;
   }
 }
--- lib/ray_world_game.dart
+++ lib/ray_world_game.dart
@@ -1,21 +1,52 @@
+import 'dart:ui';
+
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
 
 import 'helpers/direction.dart';
+import 'helpers/map_loader.dart';
 
 import 'components/player.dart';
+import 'components/world.dart';
+import 'components/world_collidable.dart';
 
 // This will be the heart of your game.
 // You’ll create and manage all your other components from here.
-class RayWorldGame extends FlameGame {
+class RayWorldGame extends FlameGame with HasCollisionDetection {
   final Player _player = Player();
+  final World _world = World();
 
   void onJoypadDirectionChanged(Direction direction) {
     _player.direction = direction;
   }
 
+  // Here, you use a helper function, MapLoader, to read
+  // rayworld_collision_map.json, located in your assets folder.
+  // For each rectangle, it creates a WorldCollidable and adds it to your game.
+  void addWorldCollision() async =>
+      (await MapLoader.readRayWorldCollisionMap()).forEach((rect) {
+        add(WorldCollidable()
+          ..position = Vector2(rect.left, rect.top)
+          ..width = rect.width
+          ..height = rect.height);
+      });
+
   @override
   Future<void> onLoad() async {
+    await add(_world);
+    // You must load the world completely before loading your player.
+    // If you add the world afterward, it will render on top of your Player
+    // sprite, obscuring it.
     add(_player);
+    addWorldCollision();
+
+    // For your player to traverse the world properly, you’ll want the game
+    // viewport to follow the main character whenever they move.
+    _player.position = _world.size / 2;
+    camera.followComponent(
+      _player,
+      worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y),
+    );
   }
 }
 
